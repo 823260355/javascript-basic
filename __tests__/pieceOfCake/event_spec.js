a01ca911de144b251783448c13df19e4f9ca760f
LiYuan1229 <823260355@qq.com>
2020-09-08 20:12:35 +0800

    finish event_spec.js

--- __tests__/pieceOfCake/event_spec.js
+++ __tests__/pieceOfCake/event_spec.js
@@ -16,7 +16,7 @@ describe('for event', () => {
 
     // <--start
     // Please add the event listener to handle `click` event on `element`.
-
+    element.addListener('click',onClick);
     // --end->
 
     element.emit('click');
@@ -32,9 +32,10 @@ describe('for event', () => {
 
     waitForEvents()
       .then(() => {
+       
         // <--start
         // Please write down the correct value. You should write the final result directly.
-        const expected = undefined;
+        const expected =  [ 'I have been clicked', 'I have been clicked' ];        ;
         // --end->
 
         expect(logs).toEqual(expected);
@@ -55,9 +56,10 @@ describe('for event', () => {
 
     waitForEvents()
       .then(() => {
+        console.log('Debug: ',logs);
         // <--start
         // Please write down the correct value. You should write the final result directly.
-        const expected = undefined;
+        const expected = [ 'I have been clicked' ];
         // --end->
 
         expect(logs).toEqual(expected);
